8d418e58b54a9c094993a24f3e8eb9f6ff933210
AmilSrinath <amilsrinath5@gmail.com>
2025-08-17 14:57:27 +0530

    change about file

--- src/containers/PageAbout/PageAbout.tsx
+++ src/containers/PageAbout/PageAbout.tsx
@@ -28,7 +28,7 @@ const PageAbout: FC<PageAboutProps> = ({ className = "" }) => {
       <div className="container py-16 lg:py-28 space-y-16 lg:space-y-28">
         <SectionHero
           rightImg={rightImg}
-          heading="👋 About Us."
+          heading="👋 About Us"
           btnText=""
           subHeading="We’re impartial and independent, and every day we create distinctive, world-class programmes and content which inform, educate and entertain millions of people in the around the world."
         />
